2adc3d48fda58844302655d93339d4124828ad65
Sharadhi Shedigumme <Sharadhi.Shedigumme@skill-house.com>
2024-02-28 14:42:58 +0530

    Add project files.

---
+++ EventManagementSystem/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace EventManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            Login = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Screenshot_2024_02_27_144310;
+            pictureBox1.Location = new Point(181, 32);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(199, 172);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe Script", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.HighlightText;
+            label1.Location = new Point(143, 226);
+            label1.Name = "label1";
+            label1.Size = new Size(285, 53);
+            label1.TabIndex = 1;
+            label1.Text = "Event Manager";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Desktop;
+            textBox1.Location = new Point(127, 378);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(312, 31);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Desktop;
+            textBox2.Location = new Point(127, 494);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(312, 31);
+            textBox2.TabIndex = 3;
+            // 
+            // Login
+            // 
+            Login.BackColor = Color.DarkGray;
+            Login.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Login.ForeColor = Color.Red;
+            Login.Location = new Point(156, 580);
+            Login.Name = "Login";
+            Login.Size = new Size(254, 58);
+            Login.TabIndex = 4;
+            Login.Text = "Login";
+            Login.UseVisualStyleBackColor = false;
+            Login.Click += Login_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe Script", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.HighlightText;
+            label2.Location = new Point(127, 322);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 33);
+            label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe Script", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.HighlightText;
+            label3.Location = new Point(127, 322);
+            label3.Name = "label3";
+            label3.Size = new Size(131, 37);
+            label3.TabIndex = 6;
+            label3.Text = "UserName";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe Script", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.HighlightText;
+            label4.Location = new Point(127, 447);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 37);
+            label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe Script", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.HighlightText;
+            label5.Location = new Point(127, 447);
+            label5.Name = "label5";
+            label5.Size = new Size(126, 37);
+            label5.TabIndex = 8;
+            label5.Text = "Password";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Rockwell", 12F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.HighlightText;
+            label6.Location = new Point(239, 690);
+            label6.Name = "label6";
+            label6.Size = new Size(79, 29);
+            label6.TabIndex = 9;
+            label6.Text = "Reset";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(64, 366);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(57, 60);
+            pictureBox2.TabIndex = 10;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(73, 476);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(48, 60);
+            pictureBox3.TabIndex = 11;
+            pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Desktop;
+            ClientSize = new Size(565, 797);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(Login);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button Login;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+    }
+}
